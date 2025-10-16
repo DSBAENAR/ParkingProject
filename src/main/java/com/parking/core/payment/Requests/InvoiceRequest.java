b46e173bf6b57e6328ad7bb8d914ff7ca0624e0b
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-10-15 21:42:15 -0500

    Refactor InvoiceRequest to include product parameter and improve documentation

--- src/main/java/com/parking/core/payment/Requests/InvoiceRequest.java
+++ src/main/java/com/parking/core/payment/Requests/InvoiceRequest.java
@@ -1,14 +1,18 @@
 package com.parking.core.payment.Requests;
 
 import com.parking.core.enums.Currencies;
+import com.parking.core.model.Product;
+
 
 /**
- * Represents a request to create an invoice.
+ * Represents a request to generate an invoice.
  *
- * @param customer The name or identifier of the customer for whom the invoice is being created.
- * @param currency The currency in which the invoice will be issued, represented by the {@link Currencies} enum.
+ * @param customer The name or identifier of the customer requesting the invoice.
+ * @param currency The currency in which the invoice will be issued.
+ * @param product  The product for which the invoice is being requested.
  */
 public record InvoiceRequest(
     String customer,
-    Currencies currency
+    Currencies currency,
+    Product product
 ) {}
