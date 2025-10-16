cf8d3bfd80d8d715273b66bd31e7e6973a93b82a
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-10-15 21:44:22 -0500

    refactor: Remove unused imports from CoreApplication

--- src/main/java/com/parking/core/CoreApplication.java
+++ src/main/java/com/parking/core/CoreApplication.java
@@ -2,14 +2,6 @@ package com.parking.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import com.parking.core.payment.response.BalanceResponse;
-import com.parking.core.payment.response.InvoiceResponse;
 
 
 
